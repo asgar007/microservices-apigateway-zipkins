e473018dd58a12361206512b0f30a3b2058dc7f7
asgar007 <90379845+asgar007@users.noreply.github.com>
2023-11-21 08:29:52 +0530

    Add files via upload

---
+++ department-service/department-service/src/main/java/com/practice/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package com.practice.departmentservice.client;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import com.practice.departmentservice.model.Employee;
+
+import java.util.List;
+
+@HttpExchange
+public interface EmployeeClient {
+
+    @GetExchange("/employee/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+
+}
---
+++ department-service/department-service/src/main/java/com/practice/departmentservice/model/Employee.java
@@ -0,0 +1,5 @@
+package com.practice.departmentservice.model;
+
+public record Employee(Long id, Long departmentId, String name, int age, String position) {
+
+}
